f64eca13b72186c2731eac1b4ee5d25f9128a9c6
Kevin Lewis <kevin.k.lewis@gmail.com>
2022-08-28 16:58:43 -0600

    Add gateway and test

---
+++ lib/blob_store/gateway.ex
@@ -0,0 +1,24 @@
+defmodule BlobStore.Gateway do
+  @moduledoc """
+  Interface to the blob store.
+  """
+
+  alias BlobStore.Blob
+
+  @doc """
+  Get a blob, identified by the given bucket and name, from the blob store.
+  """
+  @spec get_blob(BlobStore.t(), bucket :: String.t(), name :: String.t()) ::
+          {:ok, Blob.t()} | {:error, any()}
+  def get_blob(blob_store, bucket, name) do
+    BlobStore.get(blob_store, bucket, name)
+  end
+
+  @doc """
+  Store a new lub from the blob store.
+  """
+  @spec put_blob(BlobStore.t(), Blob.t()) :: {:ok, Blob.t()} | {:error, any()}
+  def put_blob(blob_store, %Blob{bucket: bucket, name: name, content: content}) do
+    BlobStore.put(blob_store, bucket, name, content)
+  end
+end
---
+++ test/blob_store/gateway_test.exs
@@ -0,0 +1,15 @@
+defmodule BlobStore.GatewayTest do
+  use ExUnit.Case
+
+  alias BlobStore.Blob
+  alias BlobStore.Gateway
+  alias BlobStore.TestBlobStore
+
+  test "put/get" do
+    blob = %Blob{bucket: "bucket", name: "name", content: "content"}
+    blob_store = %TestBlobStore{put_response: {:ok, blob}, get_response: {:ok, blob}}
+
+    assert {:ok, ^blob} = Gateway.put_blob(blob_store, blob)
+    assert {:ok, ^blob} = Gateway.get_blob(blob_store, "bucket", "name")
+  end
+end
